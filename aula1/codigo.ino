48fc118b2dd8f5ea07ae761e8c78f3e23eddd4d7
Henrique Carbonari Fucks <79269823+fucks-henrique@users.noreply.github.com>
2022-02-21 13:09:44 -0300

    Create codigo.ino

---
+++ aula1/codigo.ino
@@ -0,0 +1,14 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
