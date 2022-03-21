f784217bedace2648ba2839c92e6946b98e80ecc
Henrique Carbonari Fucks <79269823+fucks-henrique@users.noreply.github.com>
2022-03-21 12:21:01 -0300

    Update codigo.ino

--- aula/2/codigo.ino
+++ aula/2/codigo.ino
@@ -1,1 +1,14 @@
-
+void setup () 
+{
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+void loop()                  
+ {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  delay(1000);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  delay(1000);
+   }
